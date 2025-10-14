5d0812e6821f5883a41d1f14bc2604cff582c745
kafui.kafuiinul <kafui.kafuiinul@gmail.com>
2025-10-14 20:29:13 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -10,23 +10,21 @@
     # pkgs.python311
     # pkgs.python311Packages.pip
     pkgs.nodejs_22
-    pkgs.android-sdk
-    pkgs.nodePackages.nodemon
+    # pkgs.nodePackages.nodemon
   ];
 
-  # Sets environment variables in the workspace
-  env = {
-    ANDROID_HOME = "${pkgs.android-sdk}";
-  };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
     ];
-    
+
     # Enable previews
     previews = {
       enable = true;
     };
   };
+
+  # Enable the Android platform
+  platforms.android.enable = true;
 }
